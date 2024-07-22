5976c1fd05cb7886d7e0e196a8a9fc5c1ee20ed1
Soumyadeep Ghosh <139796191+SoumyadeepGhosh16@users.noreply.github.com>
2024-07-22 18:14:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Frontend Developer","Programming enthusiast","Problem solver"],
+    typeSpeed:50,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
